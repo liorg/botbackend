a72be1e66e9061621ba5c073db4e593f6cef5456
lior grossman <lior.grosman@gmail.com>
2026-04-29 11:18:16 +0000

    9

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from dotenv import load_dotenv
 
 # Import centralized logging
 from logging_config import get_logger, logging_middleware
-version="1.0.8"
+version="1.0.9"
 load_dotenv()  # ← חייב להיות לפני הכל
 
 logger = get_logger("main")
