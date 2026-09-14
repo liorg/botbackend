a0a02d1e83a3beed7acc13a24020f1ca8bb1e1c4
lior grossman <lior.grosman@gmail.com>
2026-09-14 10:26:23 +0300

    Bump version from 1.0.7.8 to 1.0.7.9

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from logging_config import get_logger, logging_middleware
 
 load_dotenv()
 
-version = "1.0.7.8"
+version = "1.0.7.9"
 logger = get_logger("main")
 
 BACKEND_URL = os.getenv("BACKEND_URL", "https://vid.michal-solutions.com/api").rstrip("/")
